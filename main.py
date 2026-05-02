7dc5be6a1b811b8773a0699fda5b966c148bef00
João Pinheiro <pinha.pinheiro97@gmail.com>
2026-05-02 11:14:25 +0100

    Criação menu, função de criar registos gerais, e funções criar bandas. Funções ainda não completas

--- main.py
+++ main.py
@@ -40,3 +40,77 @@ bookings = [
     }
 ]
 
+# Criar registos é um sub-menu que terá funções adicionais para cada tipo de lista 
+def criar_registo(bands, events, bookings):
+    while True:
+        print("1 - Criar nova banda.")
+        print("2 - Criar novo evento.")
+        print("3 - Criar novo agendamento.")
+        print("4 - Voltar atrás")
+
+        choice = input("Escolha uma das opções.")
+
+        match choice:
+            case "1":
+            # Função de criar banda
+                pass 
+            case "2":
+                # Função criar event
+                pass
+            case "3":
+                # função criar agendamento
+                pass
+            case "4":
+                break
+            case _:
+                print("Insere uma das opções válidas.")
+
+
+
+# criar banda
+def criar_bandas(bands:list):
+    nome_banda = input("Nome da banda: ")
+    numeros_membros = int(input("Número de membros: "))
+    genero = input("Género musical: ")
+    contacto = input("Contacto telefónico: ")
+    email = input("Email de contacto: ")
+
+    while True:
+        resposta_disponibilidade = input("Está disponível? (s/n): ").strip().lower()
+        if resposta_disponibilidade == "s":
+            disponivel = True
+            break
+        elif resposta_disponibilidade == "n":
+            disponivel = False
+            break
+        else:
+            print("Resposta inválida. Escreve (s) ou (n).")
+
+
+    id=len(bands) + 1
+    band = {
+        "id": id,
+        "nome": nome_banda,
+        "numeros_membros": numeros_membros,
+        "genero": genero,
+        "contacto": contacto,
+        "email": email,
+        "disponivel": disponivel
+    }
+
+    bands.append(band)
+    print("Banda criada com sucesso.")
+
+
+#Menu esqueleto do mesmo
+def menu():
+    while True:
+        print("***Sistema de gestão da Agência***")
+        print("1 - Criar novo registo")
+        print("2 - Listar registos")
+        print("3 - Pesquisa avançada.")
+        print("4 - Editar registo.")
+        print("5 - Eliminar registos.")
+        print("6 - Ordenar registos.")
+        print("7 -Guardar.")
+        print("8 - Sair")
